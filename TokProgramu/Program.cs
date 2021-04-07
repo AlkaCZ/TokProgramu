7fcd7a1c01c85666f41a5a6b5bbc99a84476b71e
Petr Jelínek <jelinek.petr.2017@skola.ssps.cz>
2021-04-07 13:26:52 +0200

    Přidejte soubory projektu.

---
+++ TokProgramu/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TokProgramu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vypocty vypocty = new Vypocty();
+            Console.WriteLine("Napiš X");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Napiš N");
+            int n = Convert.ToInt32(Console.ReadLine());
+            vypocty.ForType(x, n);
+        }
+
+    }
+  public class Vypocty
+    {
+        double Factorial(int A)
+        {
+            if (A == 1 || A == 0) return 1;
+            else return A * Factorial(A - 1);
+        }
+      public  void ForType(int x, int n)
+        {
+            double Vysledek = 1;
+
+
+            for (int s = 0; s < n; s++)
+            {
+                if (s % 2 == 0)
+                {
+                    Vysledek -= (x ^ n) / Factorial(n);
+
+                }
+                else
+                {
+                    Vysledek += (x ^ n) / Factorial(n);
+
+                }
+            }
+            Console.WriteLine(Vysledek);
+            Console.ReadLine();
+        }
+    }
+}
