efe870f342051d237f20f58d65e373c3c9fce81b
Petr Jelínek <jelinek.petr.2017@skola.ssps.cz>
2021-04-07 18:06:10 +0200

    Přidány varianty DoWhile a While

--- TokProgramu/Program.cs
+++ TokProgramu/Program.cs
@@ -12,10 +12,13 @@ namespace TokProgramu
         {
             Vypocty vypocty = new Vypocty();
             Console.WriteLine("Napiš X");
+
             int x = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Napiš N");
             int n = Convert.ToInt32(Console.ReadLine());
-            vypocty.ForType(x, n);
+            //vypocty.ForType(x, n);
+            //vypocty.WhileType(x, n);
+            vypocty.DoWhileType(x, n);
         }
 
     }
@@ -44,6 +47,52 @@ namespace TokProgramu
 
                 }
             }
+            Console.WriteLine(Vysledek);
+            Console.ReadLine();
+        }
+        public void WhileType(int x, int n)
+        {
+            double Vysledek = 1;
+            int pozice = 1;
+
+            while(pozice < n)
+            {
+                if (pozice % 2 == 0)
+                {
+                    Vysledek -= (x ^ n) / Factorial(n);
+
+                }
+                else
+                {
+                    Vysledek += (x ^ n) / Factorial(n);
+
+                }
+                pozice++;
+            }
+            Vysledek += (x ^ n) / Factorial(n);
+            Console.WriteLine(Vysledek);
+            Console.ReadLine();
+        }
+        public void DoWhileType(int x, int n)
+        {
+
+            double Vysledek = 1;
+            int pozice = 1;
+            do
+            {
+                if (pozice % 2 == 0)
+                {
+                    Vysledek -= (x ^ n) / Factorial(n);
+
+                }
+                else
+                {
+                    Vysledek += (x ^ n) / Factorial(n);
+
+                }
+                pozice++;
+            }
+            while (pozice < n);
             Console.WriteLine(Vysledek);
             Console.ReadLine();
         }
